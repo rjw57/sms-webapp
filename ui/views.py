7ad5a848e20dbb61003870ca4b2aedd04659d772
Rich Wareham <rjw57@cam.ac.uk>
2018-07-10 09:28:08 +0100

    DO NOT MERGE: testing

--- ui/views.py
+++ ui/views.py
@@ -22,5 +22,15 @@ class MediaView(api.views.MediaView):
     def get(self, request, media_key):
 
         response = super().get(request, media_key)
+        response.data['title'] = 'HACK \(x^2\): ' + response.data['title']
+        response.data['description'] = r"""
+In **mathematics**, the *quadratic formula* is the solution to
+<m>ax^2 + bx + c</m> in terms of <m>x</m>. <span>foo</span>:
+
+<m>
+x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}.
+</m>
+
+""" + response.data['description']
         response.data['media_item_json'] = json.dumps(response.data)
         return response
